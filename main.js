cc7f7e1724e1733d507a7dbbd1071d236040fe35
Max Gudmundson <maxgud@gmail.com>
2018-08-22 19:49:20 -0600

    committing for safety

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 document.addEventListener("DOMContentLoaded", function(){
 	//the entire game is going to be here
 	//variables
+	var player
 	var name = document.getElementById('name')
 	var startButton = document.getElementById('start button')
+	var top = document.getElementById('top')
 	name.addEventListener('keyup', function(event){
-		if (event.target.value !== ""){
-			startButton.className=""	
+		if (event.target.value !== ''){
+			startButton.className=''	
 		} else {
-			startButton.className="hide"
+			startButton.className='hide'
 		}
 		
 	})
 
 
+
+
 	//functions
-})
+		function startGame(){
+			player = name.value
+			startButton.className = "hide"
+			var label = document.createElement('p')
+			label.innerHTML = 'Welcome ' + player
+			label.className = 'center'
+			top.append(label)
+		}
+
+		startButton.addEventListener('click', startGame)
+		})
+
+	// git fetch
+	// git rebase origin/master
+	// git branch
