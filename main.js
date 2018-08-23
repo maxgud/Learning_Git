7cb5f8cd631dcdb41c70cf3034dfbe7a4dc63844
Max Gudmundson <maxgud@gmail.com>
2018-08-22 19:28:40 -0600

    updated JS

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
 	//the entire game is going to be here
 	//variables
-	
-	
+	var name = document.getElementById('name')
+	var startButton = document.getElementById('start button')
+	name.addEventListener('keyup', function(event){
+		if (event.target.value !== ""){
+			startButton.className=""	
+		} else {
+			startButton.className="hide"
+		}
+		
+	})
+
 
 	//functions
 })
