d8a11ad267723eff2dde5f0347d9833192cebe0f
Max Gudmundson <maxgud@gmail.com>
2018-08-22 20:04:08 -0600

    clone stuff

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", function(){
 	var name = document.getElementById('name')
 	var startButton = document.getElementById('start button')
 	var top = document.getElementById('top')
+	var gameArea = document.getElementById('game-area')
+	var numGuesses = 0
+	var correctGuesses = 0
+	var ballPosition
+	var userGuess
 	name.addEventListener('keyup', function(event){
 		if (event.target.value !== ''){
 			startButton.className=''	
@@ -18,6 +23,38 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 	//functions
+
+	function genBallPosition(){
+		// generate winning location
+
+	}
+
+	function incCounter() {
+		// if correct guess add to correct guess
+		// if wrong guess add to wrong
+		// update scoreboard
+
+	}
+
+	function calcAverage() {
+		// winning / total attempts
+
+	}
+
+	function makeGuess(){
+		genBallPosition()
+	}
+
+		function drawBoxes(){
+			for(var i = 0; i < 3; i++){
+				var box = document.createElement('div')
+				box.className='box'
+				box.id = 'box-' + i
+				gameArea.append(box)
+				box.addEventListener('click', makeGuess)
+			}
+		}
+
 		function startGame(){
 			player = name.value
 			startButton.className = "hide"
@@ -25,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function(){
 			label.innerHTML = 'Welcome ' + player
 			label.className = 'center'
 			top.append(label)
+			drawBoxes()
 		}
 
 		startButton.addEventListener('click', startGame)
